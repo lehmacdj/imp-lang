9f542e38bc852d8395065363fa31068c447b1b69
Devin Lehmacher <lehmacdj@gmail.com>
2017-02-16 01:27:06 -0500

    Create repl for IMP
    - needed to update cabalfile to include all of the modules
    - also added some dependencies for the executable

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,17 @@ module Main where
 
 import Language.IMP
 
+import Control.Monad
+import Control.Monad.Trans.Class
+import System.Console.Haskeline
+
+until_ :: Monad m => (a -> Bool) -> m (Maybe a) -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+    result <- prompt
+    case mfilter (not . pred) result of
+      Nothing -> pure ()
+      Just result' -> action result' >> until_ pred prompt action
+
 main :: IO ()
-main = undefined
+main = runInputT defaultSettings $
+    until_ (==":q") (getInputLine "imp> ") (lift . either print print . evaluate)
